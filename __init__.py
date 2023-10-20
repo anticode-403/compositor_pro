c6a389f74fb74b3aa8b3b259bf5baa8db498d136
rileyanticode@gmail.com <rileyanticode@gmail.com>
2023-10-20 16:33:57 -0700

    Increment version number

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ bl_info = {
     "name" : "Compositor Pro",
     "author" : "anticode-403, Nihal Rahman",
     "blender" : (3, 6, 0),
-    "version" : (0, 0, 6),
+    "version" : (0, 0, 7),
     "category" : "Compositing"
 }
 
