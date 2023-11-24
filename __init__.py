5c262a53f8f3006e8e0def210b27774002701dab
Riley Rivera <rileyanticode@gmail.com>
2023-11-24 15:33:49 -0800

    Increment version

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ bl_info = {
     "author" : "anticode-403, Nihal Rahman",
     "location": "Blender Compositor",
     "blender" : (3, 6, 0),
-    "version" : (0, 5, 0),
+    "version" : (0, 5, 1),
     "category" : "Compositing",
     # "doc_url": "https://comppro.anticode.me/", # Docs aren't ready.
 }
